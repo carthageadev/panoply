90bbbef43ba7ba4255e745fe98e25e3520268175
AhmedBenAbdallahDev <AhmedDev@email.com>
2026-07-15 19:46:52 +0100

    Set local Vite development port

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
+    port: 5174,
     proxy: {
       '/api2': {
         target: 'https://www.screenscraper.fr',
